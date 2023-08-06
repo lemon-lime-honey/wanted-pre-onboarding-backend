0ea8f1e74f16a59ff7dbf54d452e3ac3ed196ef7
lemon-lime-honey <bs0308@g.skku.edu>
2023-08-06 09:58:20 +0900

    Add apps

--- cfg/settings.py
+++ cfg/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'posts',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

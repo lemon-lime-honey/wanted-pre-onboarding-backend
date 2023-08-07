d6ca7323d7fffe12b9bc90dc97167742ba17b4e0
lemon-lime-honey <bs0308@g.skku.edu>
2023-08-07 17:18:27 +0900

    Add simple-JWT

--- cfg/settings.py
+++ cfg/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'accounts',
     'posts',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}

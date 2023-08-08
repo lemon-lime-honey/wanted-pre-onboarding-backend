85989fe1230a95a67197dab7e6b8791614cffa7f
lemon-lime-honey <bs0308@g.skku.edu>
2023-08-08 12:07:52 +0900

    Update password validation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
+import re
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class SignupSerializer(serializers.ModelSerializer):
@@ -9,6 +11,11 @@ class SignupSerializer(serializers.ModelSerializer):
             password=validated_data['password']
             )
         return user
+
+
+    def validate_password(self, password):
+        if len(password) < 8:
+            raise serializers.ValidationError(_("Password must be longer than 7 letters."))
 
 
     class Meta:

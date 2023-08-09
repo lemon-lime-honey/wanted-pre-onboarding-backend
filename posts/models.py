15c30b1ab14cd2611db918ec3eab409fcaa8fda9
lemon-lime-honey <bs0308@g.skku.edu>
2023-08-09 18:46:50 +0900

    Update posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    pagination_class = PageNumberPagination
+
+
+    def get_permissions(self):
+        if self.action in ('create', 'update', 'destroy'):
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        else:
+            return PostSerializer
+
+
+    def create(self, request):
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+    def update(self, request, pk):
+        post = self.queryset.get(pk=pk)
+
+        if request.user != post.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(instance=post, data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def destroy(self, request, pk):
+        post = self.queryset.get(pk=pk)
+
+        if request.user != post.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
